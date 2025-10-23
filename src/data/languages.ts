c8072591e9132729f856559822ffb50033b386e2
Ronald Mirabal <ronaldmirabal@outlook.es>
2025-10-23 10:10:55 -0400

    mod

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -7,29 +7,18 @@ interface Language {
 
 const languages: Language[] = [
     {
-        name: "English",
+        name: "Spanish",
         level: "Native",
         description: "I speak fluently and write fluently",
         show: true
     },
     {
-        name: "Spanish",
-        level: "Bilingual",
-        description: "I speak fluently and write fluently",
-        show: true
-    },
-    {
-        name: "French",
+        name: "English",
         level: "B1",
         description: "I speak fluently and write fluently",
         show: true
-    },
-    {
-        name: "Italian",
-        level: "B2",
-        description: "I speak fluently and write fluently",
-        show: false
     }
+
 ];
 
 export default languages;
